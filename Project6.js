4a933525fa7ea48464259da5ee18d1551917152b
DubstepKnight <nursmaster@gmail.com>
2018-12-14 13:16:33 +0300

    some tweaks to Fibonacci Sequence

--- Project6.js
+++ Project6.js
@@ -1,6 +1,11 @@
 function Fibonacci_sequence()
 {
     var maximum = document.getElementById("maximum").value;
+    var reference;
+    if (maximum == 2019)
+    {
+        document.getElementById("reference").innerHTML = "Happy New 2019 Year" ;
+    }
     var range = [];
     range[0] = 1;
     range[1] = 1;
